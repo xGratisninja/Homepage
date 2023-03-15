3ef3d2492f74c20157eb98bfd1b151f8d3f60fa0
xGratisninja <67469587+xGratisninja@users.noreply.github.com>
2023-03-15 21:40:35 +0100

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+function openMenu() {
+    const Btn = document.getElementById('SideMenuBtn');
+    const Menu = document.getElementById('SideMenu');
+    let buttonStatus = Btn.textContent
+    if (buttonStatus == "❮") {
+        Btn.innerHTML = "&#10095;"
+        Btn.style.marginRight = "200px"
+        Menu.style.marginRight = "0px"
+    }
+    else {
+        Btn.innerHTML = "&#10094;"
+        Btn.style.marginRight = "0px"
+        Menu.style.marginRight = "-200px"
+    }
+    //console.log(buttonStatus)
+
+}
